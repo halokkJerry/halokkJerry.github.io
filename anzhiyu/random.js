570b4960f1402eb8fd1b16de6a39725cac4fea26
halokkJerry <905173830@qq.com>
2024-01-15 14:14:27 +0800

    Site updated: 2024-01-15 14:14:27

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,3 +1,3 @@
-var posts=["2024/01/03/GO避坑指南「一」/","2024/01/02/hello-world/","2024/01/02/线性代数「一」行列式/","2024/01/04/线性代数「二」矩阵及其运算/"];function toRandomPost(){
+var posts=["2024/01/03/GO避坑指南「一」/"];function toRandomPost(){
     pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
   };
